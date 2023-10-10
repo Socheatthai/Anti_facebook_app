6f334275b5341ce0c6325e645a725641ec2d5362
Socheatthai <thaisocheat.123@gmail.com>
2023-10-10 22:27:11 +0700

    fix

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello LLL </Text>
+      <Text>dfsfdfdjf </Text>
       <StatusBar style="auto" />
     </View>
   );
