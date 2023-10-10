e4f9c60156f7e56c5015b1d93532519e052e5e5b
Socheatthai <thaisocheat.123@gmail.com>
2023-10-10 22:22:01 +0700

    second

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello wolrd</Text>
+      <Text>Hooov dfds </Text>
       <StatusBar style="auto" />
     </View>
   );
